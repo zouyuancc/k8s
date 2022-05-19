3f51b4683cb89a8eed2f1e16612169516113892e
zouyuan <zouyuancc@gmail.com>
2022-05-19 10:04:45 +0800

    input ip from command for main.go

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
-import "k8s/common"
+import (
+	"flag"
+	"k8s/common"
+)
+
+var realip string
 
 func main() {
+	flag.StringVar(&realip, "rip", "127.0.0.1", "Input the server ip,default 127.0.0.1")
+	flag.Parse()
 	done := make(chan bool)
-	server := common.NewServer("127.0.0.1", 20000, 20001)
+	server := common.NewServer(realip, 20000, 20001)
 	go server.Start(done)
 	<-done
 }
